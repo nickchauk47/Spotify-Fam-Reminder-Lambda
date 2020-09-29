e59633556ed642a4419c5c772b17151b7540bb87
nickchauk47 <nickchauk47@users.noreply.github.com>
2020-09-28 23:32:31 -0700

    Added comment to test github action

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 exports.handler = async (event) => {
     // TODO implement
+    // TESTING GITHUB ACTION 1
     const response = {
         statusCode: 200,
         body: JSON.stringify('GITHUB ACTION WORKS!'),
