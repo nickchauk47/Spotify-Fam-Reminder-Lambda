eef86d662ecd68c7659f272d57c882bb2fb7e3a2
nickchauk47 <nickchauk47@users.noreply.github.com>
2020-09-29 02:20:21 -0700

    testing action and removed comment from yml

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 exports.handler = async (event) => {
     // TODO implement
-    // TESTING GITHUB ACTION 1
+    // TESTING GITHUB ACTION 2
     const response = {
         statusCode: 200,
         body: JSON.stringify('GITHUB ACTION WORKS!'),
