012eec163622adb6290ba987346bd770425395f7
nickchauk47 <nickchauk47@users.noreply.github.com>
2020-09-29 02:53:58 -0700

    hard reset

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-
+let AWS = require('aws-sdk');
+AWS.config.update({region: 'us-west-1'});
 
 exports.handler = async (event) => {
-    // TODO implement
-    // TESTING GITHUB ACTION 2
+
+  let params = {
+    Message: 'Pay for yo Spotify bitch',
+    PhoneNumber: '+5555555',
+  };
+  
+  let publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
+  
+  // Handle promise's fulfilled/rejected states
+  await publishTextPromise.then( function(data) {
+    console.log("MessageID is " + data.MessageId);
     const response = {
-        statusCode: 200,
-        body: JSON.stringify('GITHUB ACTION WORKS!'),
+      statusCode: 200,
+      body: JSON.stringify("MessageID is " + data.MessageId)
     };
+    console.log(response);
     return response;
+  }).catch(
+    function(err) {
+      const response = {
+        statusCode: 500,
+        body: JSON.stringify("Error: " + err)
+      };
+      console.log(response);
+      return response;
+    }
+  );
 };
